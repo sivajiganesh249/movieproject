4765a14f54d088a452341d6d080639224a506108
SIVAJI GANESH <sankar@jinka.gov>
2021-04-05 22:45:46 -0400

    my first commit

--- movieproject/testapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from testapp.models import Movie
-# Register your models here.
-class MovieAdmin(admin.ModelAdmin):
-    list_display=['rdate','moviename','hero','heroine','rating']
-admin.site.register(Movie,MovieAdmin)
--- movieproject/testapp/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from testapp.models import Movie
-class MovieForm(forms.ModelForm):
-    class Meta:
-        model=Movie
-        fields='__all__' 
